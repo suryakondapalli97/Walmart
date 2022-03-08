21d281392fecd114edb563a163b3293f51acb58e
suryakondapalli97 <101154561+suryakondapalli97@users.noreply.github.com>
2022-03-07 21:33:24 -0500

    Commits

---
+++ config.py
@@ -0,0 +1,5 @@
+//Using environment variables to get all the consumer keys and access tokens.
+consumer_key = os.environ.get('C_KEY')
+consumer_secret = os.environ.get('C_SECRET')
+access_token = os.environ.get('TOKEN')
+access_token_secret = os.environ.get('TOKEN_SECRET')
